26077a1116925dea93f6233cdf6b7ab7fd60eefc
Olha_Khodak <olgapolina261111@ukr.net>
2025-11-01 19:00:20 +0200

    create structure, add styles for components, add App and CafeInfo

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
+
+export default function App() {
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  );
+}
